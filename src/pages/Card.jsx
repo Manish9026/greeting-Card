49d20c7d83caff23362d14cdb9c8fbf82272c4e7
Manish9026 <135260553+Manish9026@users.noreply.github.com>
2024-04-17 23:12:43 +0530

    add new page

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -11,13 +11,14 @@ const Card = () => {
     const [page1,setPage1]=useState(1);
     const [page2,setPage2]=useState(1);
     const [page3,setPage3]=useState(1);
+
    useState(()=>{
     console.log(pageRef);
    },[])
   return (
     <div className="birthdayCard" >
 
-<div className={page1?"cardFront":"cardFront page1"} >
+<div  style={{zIndex:3000}} className={page1?"cardFront":"cardFront page1"} >
   {/* <div className="front-text">
   <h3 className="happy">HAPPY</h3>
   <h2 className="bday">BIRTHDAY</h2> 
@@ -39,14 +40,14 @@ const Card = () => {
   <TiArrowForward className='icon'  onClick={()=>setPage1(prev=>!prev)}/>
 </div>
   
-  <div style={{zIndex:2000}}className={page2?"cardInside  ":"cardInside page2"} >
+  <div style={{zIndex:2000}} className={page2?"cardInside  ":"cardInside page2"} >
      <div className="inside-text">
   <h3 className="happy">HAPPY</h3>
   <h2 className="bday">BIRTHDAY</h2> 
   <h3 className="toyou">to you!</h3>  
   </div>
     <div className="wishes">
-    <p>Dear Friend,</p>
+    <p>Dear Friend, page 2</p>
     <p>Happy birthday!! I hope your day is filled with lots of love and laughter! May all of your birthday wishes come true.</p>
     <p className="name">xxx</p>
     </div>
@@ -56,18 +57,25 @@ const Card = () => {
   <TiArrowForward className='icon'  onClick={()=>setPage2(prev=>!prev)}/>
   </div>
 
-  <div className="cardInside page3" ref={pageRef.current.third}>
-     <div className="inside-text">
-  <h3 className="happy">HAPPY</h3>
-  <h2 className="bday">BIRTHDAY</h2> 
-  <h3 className="toyou">to you!</h3>  
-  </div>
+  <div  style={{zIndex:1000}} className={page3?"cardInside":"cardInside page3"}>
+    
     <div className="wishes">
-    <p>Dear Friend,</p>
-    <p>Happy birthday</p>
-    <p className="name">xxx</p>
+   
+    <p>Happy birthday page 3</p>
+    
     </div>
-    <TiArrowForward className='icon'  onClick={()=>setPage2(prev=>!prev)}/>
+    <TiArrowForward className='icon' style={{transform:"rotatey(180deg)",left:"10px"}} onClick={()=>setPage2(prev=>!prev)}/>
+    <TiArrowForward className='icon'  onClick={()=>setPage3(prev=>!prev)}/>
+  </div>
+
+  <div className="cardInside page4" >
+   
+    <div className="wishes">
+    
+    <p>page 4</p>
+    
+    </div>
+    <TiArrowForward className='icon'  onClick={()=>setPage3(prev=>!prev)}/>
   </div>
 </div>
   )
