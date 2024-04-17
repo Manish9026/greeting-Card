c09275840babee49a9ae669b02949f8495551ff1
Manish9026 <135260553+Manish9026@users.noreply.github.com>
2024-04-17 18:26:34 +0530

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './index.css' 
 import Card from './pages/Card'
 import video from './assets/bg2.mp4'
 const App = () => {
+const videoRef=useRef(null);
+//   useEffect(()=>{
+//     console.log(videoRef);
+// const i =setTimeout(()=>{
+
+//   console.log("hello");
+//   startVideo();
+// },1000)
+
+
+// return ()=>clearTimeout(i);
+//   },[])
+
+//   const startVideo = () => {
+//     if (videoRef.current) {
+//       videoRef.current.play();
+//     }
+//   };
+
+
   return (
     <div className='container'> 
-      <video className='video' autoPlay loop>
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
+      <video  className='video' src={video}  ref={videoRef} muted autoPlay playsInline loop >
+
       </video>
+      {/* <button onClick={()=>startVideo() } style={{background:"red" ,color:"white",zIndex:100}} > play</button> */}
       <Card/>
     </div>
   )
--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './card.css';
+import Text from './component/text/Text.jsx';
 const Card = () => {
   return (
     <div className="birthdayCard">
 <div className="cardFront">
-  <div className="front-text">
+  {/* <div className="front-text">
   <h3 className="happy">HAPPY</h3>
   <h2 className="bday">BIRTHDAY</h2> 
   <h3 className="toyou">to you!</h3>  
-  </div>
+  </div> */}
   <div className="wrap-deco">
 <div className="decorations"></div>
 <div className="decorationsTwo"></div>
@@ -17,10 +18,12 @@ const Card = () => {
 <div className="decorations"></div>
 <div className="decorationsThree"></div>
   </div>
-  <div className="plate">
+  {/* <div className="plate">
     <div className="cake"></div>
     <div className="flame"></div>
-  </div>
+  </div> */}
+
+  <Text/>
 </div>
   
   <div className="cardInside">
